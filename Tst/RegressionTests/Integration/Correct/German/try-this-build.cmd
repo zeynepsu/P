546ea03faf7694418c567502b4e2b6f1b2e39eea
Thomas Wahl <t-thwahl@microsoft.com>
2018-04-27 16:11:49 +0530

    more convergence test

---
+++ Tst/RegressionTests/Integration/Correct/German/try-this-build.cmd
@@ -0,0 +1,9 @@
+..\..\..\..\..\bld\drops\Debug\x64\Binaries\pc.exe /generate:C# german.p
+
+..\..\..\..\..\bld\drops\Debug\x64\Binaries\pc.exe /link /r:German.4ml
+
+..\..\..\..\..\bld\drops\Debug\x64\Binaries\pc.exe /generate:C# /link /r:German.4ml
+
+..\..\..\..\..\bld\drops\Debug\x64\Binaries\pt.exe /dfs linker.dll
+
+
