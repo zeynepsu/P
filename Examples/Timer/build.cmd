1ed1cafc159b8c141396f44f6cdc1655c6aac35f
Thomas Wahl <t-thwahl@microsoft.com>
2018-05-07 18:32:38 +0530

    cleanup

--- Examples/Timer/build.cmd
+++ Examples/Timer/build.cmd
@@ -1,10 +1,19 @@
-@echo pc /generate:C# Timer.p /t:Timer.4ml
-@call pc /generate:C# Timer.p /t:Timer.4ml
+@echo off
 
-@echo pc /generate:C# Main.p /t:Main.4ml /r:Timer.4ml
-@call pc /generate:C# Main.p /t:Main.4ml /r:Timer.4ml
+set pc=%PHOME%\bld\drops\Debug\x64\Binaries\pc.exe
 
-@echo pc /generate:C# /link /r:Timer.4ml /r:Main.4ml
-@call pc /generate:C# /link /r:Timer.4ml /r:Main.4ml
+echo %pc% /generate:C# Timer.p /t:Timer.4ml
+call %pc% /generate:C# Timer.p /t:Timer.4ml
 
-@echo now consider running something like "pt /dfs linker.dll"
+if NOT errorlevel 0 goto :eof
+
+echo %pc% /generate:C# Main.p /t:Main.4ml /r:Timer.4ml
+call %pc% /generate:C# Main.p /t:Main.4ml /r:Timer.4ml
+
+if NOT errorlevel 0 goto :eof
+
+echo %pc% /generate:C# /link /r:Timer.4ml /r:Main.4ml
+call %pc% /generate:C# /link /r:Timer.4ml /r:Main.4ml
+
+echo now consider running something like "pt /dfs linker.dll"
+
--- Examples/cleanup.cmd
+++ Examples/cleanup.cmd
@@ -1,6 +1,8 @@
-@del *.4ml      2> nul
-@del *.cs       2> nul
-@del *.dll      2> nul
-@del linker.pdb 2> nul
-@del *~         2> nul
+@echo off
+
+del *.4ml      2> nul
+del *.cs       2> nul
+del *.dll      2> nul
+del linker.pdb 2> nul
+del *~         2> nul
 
