dc2741a59673f3536712cb51b67a8f80199ec572
Suvam Mukherjee <t-sumukh@microsoft.com>
2018-09-05 09:22:08 +0530

    more additions to vsc

---
+++ Src/Pc/CompilerCore/Backend/Solidity/SolidityCodeGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Pc.Backend.ASTExt;
+using Microsoft.Pc.TypeChecker;
+using Microsoft.Pc.TypeChecker.AST;
+using Microsoft.Pc.TypeChecker.AST.Declarations;
+using Microsoft.Pc.TypeChecker.AST.Expressions;
+using Microsoft.Pc.TypeChecker.AST.Statements;
+using Microsoft.Pc.TypeChecker.AST.States;
+using Microsoft.Pc.TypeChecker.Types;
+
+namespace Microsoft.Pc.Backend.Solidity
+{
+    public class SolidityCodeGenerator : ICodeGenerator
+    {
+        public IEnumerable<CompiledFile> GenerateCode(ICompilationJob job, Scope globalScope)
+        {
+            var context = new CompilationContext(job);
+            CompiledFile soliditySource = GenerateSource(context, globalScope);
+            return new List<CompiledFile> { soliditySource };
+        }
+    }
+}
