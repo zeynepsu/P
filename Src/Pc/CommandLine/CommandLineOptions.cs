179fa23355e94511df7ca894861f540a571bd7f2
Suvam Mukherjee <t-sumukh@microsoft.com>
2018-09-14 11:43:59 +0530

    incremental

--- Src/Pc/CommandLine/CommandLineOptions.cs
+++ Src/Pc/CommandLine/CommandLineOptions.cs
@@ -189,6 +189,7 @@ namespace Plang.Compiler
 
         public static void PrintUsage()
         {
+
             CommandlineOutput.WriteMessage("USAGE: Pc.exe file1.p [file2.p ...] [-t:tfile] [options]",
                 SeverityKind.Info);
             CommandlineOutput.WriteMessage(
@@ -196,16 +197,14 @@ namespace Plang.Compiler
                 SeverityKind.Info);
             CommandlineOutput.WriteMessage("    -outputDir:path            -- where to write the generated files",
                 SeverityKind.Info);
-            CommandlineOutput.WriteMessage("    -generate:[C,P#]           -- select a target language to generate",
+            CommandlineOutput.WriteMessage("    -generate:[C,P#,Solidity]           -- select a target language to generate",
                 SeverityKind.Info);
             CommandlineOutput.WriteMessage("        C   : generate C code using the Prt runtime", SeverityKind.Info);
             CommandlineOutput.WriteMessage("        P#  : generate C# code using the P# runtime", SeverityKind.Info);
+            CommandlineOutput.WriteMessage("  Solidity  : generate Solidity code", SeverityKind.Info);
             CommandlineOutput.WriteMessage("    -sourcemaps[:(true|false)] -- enable or disable generating source maps",
                 SeverityKind.Info);
             CommandlineOutput.WriteMessage("                                  in the compiled output. may confuse some",
-                SeverityKind.Info);
-            CommandlineOutput.WriteMessage("                                  debuggers.", SeverityKind.Info);
-            CommandlineOutput.WriteMessage("    -h, -help, --help          -- display this help message",
                 SeverityKind.Info);
         }
     }
